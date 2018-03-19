134168bbf038019cf3392fb5ec019ffd4e19af02
lordkabal26 <lordkabal26@hotmail.com>
2018-03-19 17:03:35 -0400

    Add files via upload

---
+++ dayz_22.smd_sahrani_A2/dayz_code/configs/Category/special_Collection.hpp
@@ -0,0 +1,62 @@
+class Category_805 {
+	class Viper {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class Viper2 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};	
+	class ffs_bf109 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class ffs_bf109_sc50 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};	
+	class ffs_bf109_sc250 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class ffs_bf109_trop {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};	
+	class ffs_bf109_trop_sc50 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class ffs_bf109_trop_sc250 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};		
+	class zerosenJ {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};
+	class PRACS_M163 {
+		type = "trade_any_vehicle";
+		buy[] ={100000,"Coins"};
+		sell[] ={50000,"Coins"};
+	};	
+	class GzerosenJ {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+	class f4uJ {
+		type = "trade_any_vehicle";
+		buy[] ={200000,"Coins"};
+		sell[] ={100000,"Coins"};
+	};
+};
